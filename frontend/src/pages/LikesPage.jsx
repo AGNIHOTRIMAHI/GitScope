e3cb5f0d1d32300edb3697aad95210a177439911
MAHIAGNIHOTRI <agnihotrimahi70@gmail.com>
2025-04-10 01:10:32 +0530

    ExplorePage created

--- frontend/src/pages/LikesPage.jsx
+++ frontend/src/pages/LikesPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import {FaHeart} from "react-icons/fa"
 const LikesPage = () => {
   return (
-    <div>
+    <div className='relative overflow-x-auto shadow-md rounded-lg px-4'>
+      <table className='w-full text-sm text-left rt1:text-right bg-glass overflow-hidden border'>
+        <thead className='text-xs uppercase bg-glass '>
+           
+        </thead>
+
+      </table>
       likespage
     </div>
   )
